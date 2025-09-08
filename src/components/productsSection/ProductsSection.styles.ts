893d6b79025588654459e4b55290c4024917e7e2
Davi Marques <davimarque2000@gmail.com>
2025-09-08 03:46:20 -0300

    style:ajusta estilos de product section para mobile

--- src/components/productsSection/ProductsSection.styles.ts
+++ src/components/productsSection/ProductsSection.styles.ts
@@ -1,8 +1,16 @@
+import { media } from '@/constants';
 import styled from 'styled-components';
 
 export const PageTitle = styled.h1`
   font-size: 40px;
   font-weight: 700;
+
+  ${media.mobile} {
+    font-size: 24px;
+  }
+  ${media.tablet} {
+    font-size: 32px;
+  }
 `;
 
 export const TitleContainer = styled.div`
@@ -10,8 +18,17 @@ export const TitleContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 47px;
+  ${media.mobile} {
+    flex-direction: column;
+  }
 `;
 export const CategoryDescription = styled.span`
   font-size: 20px;
   font-weight: 400;
+  ${media.mobile} {
+    font-size: 16px;
+  }
+  ${media.tablet} {
+    font-size: 18px;
+  }
 `;
